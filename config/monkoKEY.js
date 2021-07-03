164de4f66ecceb1749bf191c84b2d5771fedd74e
DHEERAJ NAHARIYA <dheerajsharma4711@gmail.com>
2021-07-03 17:47:18 +0530

    short-url-service

---
+++ config/monkoKEY.js
@@ -0,0 +1,6 @@
+// module.exports = "mongodb://localhost:27017/url-short"
+
+module.exports = "mongodb+srv://dheeraj:dheeraj123@cluster0.o9hwz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+// mongodb+srv://dheeraj1:<dheeraj1@123>@cluster0.lqlwq.mongodb.net/URL?retryWrites=true&w=majority
+
